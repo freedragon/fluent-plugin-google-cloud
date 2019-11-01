3bcd284d525d3f6f43b9c49eb5ff39bd19357366
Yong Hee Park <hangondragon@hotmail.com>
2019-11-01 16:55:17 +0900

    Remove azure from logging during config

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -613,7 +613,8 @@ module Fluent
         @write_request = method(:write_request_via_rest)
       end
 
-      if [Platform::GCE, Platform::EC2,Platform::AZURE].include?(@platform)
+      # if [Platform::GCE, Platform::EC2, Platform::AZURE].include?(@platform)
+      if [Platform::GCE, Platform::EC2].include?(@platform)
         # Log an informational message containing the Logs viewer URL
         @log.info 'Logs viewer address: https://console.cloud.google.com/logs/',
                   "viewer?project=#{@project_id}&resource=#{@resource.type}/",
