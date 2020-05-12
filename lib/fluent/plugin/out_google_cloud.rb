43c68f64f8ad3ad3abd1e3384240a7dcbd5e2390
Yong Hee Park <hangondragon@hotmail.com>
2020-05-12 16:11:40 +0900

    Corrected syntax error

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -1202,14 +1202,13 @@ module Fluent
     # 2. If not, try to retrieve it locally.
     def set_vm_name
       @vm_name ||= Socket.gethostname
-      @vm_name ||=
-                 if @platform == Platform::AZURE
-                   if ENV['HOSTNAME'].to_s.empty?
-                     fetch_azure_metadata('instance/compute/vmId')
-                   else
-                     ENV['HOSTNAME']
+      @vm_name ||= if @platform == Platform::AZURE
+                     if ENV['HOSTNAME'].to_s.empty?
+                       fetch_azure_metadata('instance/compute/vmId')
+                     else
+                       ENV['HOSTNAME']
+                     end
                    end
-                 end
     rescue StandardError => e
       @log.error 'Failed to obtain vm name: ', error: e
     end
